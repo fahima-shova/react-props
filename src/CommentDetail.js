d1b4bb2a6429b27b3dc383af60864bb9796d9a7a
fahima-shova <fahima.shova06@gmail.com>
2022-05-24 18:49:29 +0600

    second commit

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Faker from 'faker';
-
 
 const CommentDetail = props =>{
     return (
--- src/index.js
+++ src/index.js
@@ -2,27 +2,38 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import './style/App.css';
+
 const App = () => {
     return (
-        <div className = 'ui container comments'>  {/* the whole coment section */}
-           <CommentDetail 
-              author = "Sam" 
-              timeAgo = "Today at 4:00PM" 
-              comment = "Good blog post!" 
-              image={Faker.image.image()}
+        <div className = 'ui container comments'> 
+           <ApprovalCard> 
+            <CommentDetail 
+                author = "Sam" 
+                timeAgo = "Today at 4:00PM" 
+                comment = "Good blog post!" 
+                image = {Faker.image.image()}
             /> 
-           <CommentDetail 
-              author = "Alex" 
-              timeAgo = "Today at 5:00AM" 
-              comment = "Amazing." 
-              image={Faker.image.image()}
+           </ApprovalCard>
+
+           <ApprovalCard>
+            <CommentDetail 
+                author = "Alex" 
+                timeAgo = "Today at 5:00AM" 
+                comment = "Amazing." 
+                image = {Faker.image.image()}
             /> 
-           <CommentDetail 
-              author = "Jane" 
-              timeAgo = "Yesterday at 2:00PM" 
-              comment = "Great!" 
-              image={Faker.image.image()}
+           </ApprovalCard>
+           
+           <ApprovalCard>
+            <CommentDetail 
+                author = "Jane" 
+                timeAgo = "Yesterday at 2:00PM" 
+                comment = "Great!" 
+                image = {Faker.image.image()}
             /> 
+           </ApprovalCard>
         </div>
 
     )
